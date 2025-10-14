926edf4fe29a7b005440cdf1c32a703fd574edcd
Shafqat Haider <93528873+ShafqatHaider@users.noreply.github.com>
2025-10-15 00:14:27 +0500

    finally

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -44,7 +44,6 @@ export class HeaderComponent {
 
   onLogout() {
     debugger
-    alert('cliked')
     this.logout.emit();
     this.isDropdownOpen = false;
   }
--- src/environment.ts
+++ src/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    ROOT_API_URL: 'https://localhost:7060/api'
+    // ROOT_API_URL: 'https://localhost:7060/api'
+    ROOT_API_URL: 'https://breadpos.racsapiserver.com/api'
 }
